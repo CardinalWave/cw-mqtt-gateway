de2bf5240b04398d6fe8b5209ef07d2be900135c
Marquezv <vmarquezv@outlook.com.br>
2024-05-09 17:00:43 -0300

    config: Added api.yaml document

--- src/main/adapters/mqtt_adapter.py
+++ src/main/adapters/mqtt_adapter.py
@@ -4,7 +4,7 @@ from src.domain.models.sessions import Session
 def mqtt_adapter(msg: any) -> any:
 
     parts = msg.topic.split('/')
-    payload_obj = json.loads(msg.payload.decode("utf-8"))["payload"]
+    payload_obj = json.loads(msg.payload.decode("utf-8"))
     device_id = parts[1]
     sessao = parts[2]
     action = parts[3]
--- src/presentation/sessions/user_session.py
+++ src/presentation/sessions/user_session.py
@@ -14,7 +14,7 @@ class UserSession(SessionInterface):
     def package(self):
         Session(self.device_id, self.session_id, self.action, self.user)
         response = {
-                        "device_id": self.device_id[0],
+                        "device_id": self.device_id[0][0],
                         "session_id": self.session_id[0],
                         "action": self.action[0],
                         "payload": {
