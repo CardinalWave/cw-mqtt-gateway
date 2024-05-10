e75675f1b1860ec56ab8b66b0ed880ca26f32003
Marquezv <vmarquezv@outlook.com.br>
2024-05-10 14:01:47 -0300

    config: Pylint

--- src/data/use_cases/user_login.py
+++ src/data/use_cases/user_login.py
@@ -1,10 +1,11 @@
+# pylint: disable=missing-function-docstring
 import uuid
 from src.domain.use_cases.user_login import UserLogin as UserLoginInterface
 from src.domain.models.users import User
 from src.domain.models.login import Login
 
-class UserLogin(UserLoginInterface):
 
+class UserLogin(UserLoginInterface):
     @classmethod
     def login(cls, login: Login) -> User:
         token = str(uuid.uuid4())
