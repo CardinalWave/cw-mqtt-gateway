b87bce154b8674ddf9a694903844647f91b9edf3
Marquezv <vmarquezv@outlook.com.br>
2024-05-24 21:15:44 -0300

    refact: Exception in inicialization

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import time
 from src.main.mqtt.mqtt import mqttc, on_connect, on_message, on_publish
 
 if __name__ == "__main__":
@@ -6,11 +7,16 @@ if __name__ == "__main__":
     mqtt_broker_ip = os.getenv('MQTT_BROKER_IP')
     mqtt_broker_port = int(os.getenv('MQTT_BROKER_PORT'))
 
-    mqttc.connect(mqtt_broker_ip, mqtt_broker_port, 60)
+    connected = False
 
-    mqttc.subscribe("#");
-    mqttc.on_connect = on_connect
-    mqttc.on_message = on_message
-    mqttc.on_publish = on_publish
-
-    mqttc.loop_forever()
+    while not connected:
+        try:
+            mqttc.connect(mqtt_broker_ip, mqtt_broker_port, 60)
+            mqttc.subscribe("#");
+            mqttc.on_connect = on_connect
+            mqttc.on_message = on_message
+            mqttc.on_publish = on_publish
+            mqttc.loop_forever()
+        except Exception as e:
+            print(f"Falha na conexao: {e}")
+            time.sleep(5)
