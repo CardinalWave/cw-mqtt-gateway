5a441474a77a5b4428033eaeba55afc8aacd3fa3
Marquezv <vmarquezv@outlook.com.br>
2024-08-01 20:29:18 -0300

    refact: Change group_join params

--- src/data/use_cases/group/group_join.py
+++ src/data/use_cases/group/group_join.py
@@ -1,12 +1,12 @@
 import json
 from src.domain.models.group import Group
 from src.domain.use_cases.group.group_join import GroupJoin as GroupJoinInterface
-from src.domain.use_cases.central.central_conn import CentralConn
+from src.domain.use_cases.central.central_conn import CentralConn as CentralConnInterface
 
 
 class GroupJoin(GroupJoinInterface):
 
-    def __init__(self, central_conn: CentralConn):
+    def __init__(self, central_conn: CentralConnInterface):
         self.__central_conn = central_conn
 
     def group_join(self, group_user: dict) -> Group:
@@ -16,11 +16,8 @@ class GroupJoin(GroupJoinInterface):
 
         params = json.dumps({
             'token': user.token,
-            'username': user.username,
-            'group_title': group.title,
             'group_id': group.group_id
         })
-        print(params)
         json_data = self.__central_conn.request(params=params, action="/group/join")
         group_join = Group(group_id=json_data['payload']['group_id'],
                            group_name=json_data['payload']['group_title'])
